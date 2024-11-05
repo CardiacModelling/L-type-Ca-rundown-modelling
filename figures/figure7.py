96e4ed9ce346417d5394c7ff0c3dcb7456a8e153
Aditi <aditi.agrawal@dtc.ox.ac.uk>
2024-11-05 01:13:04 +0000

    updates in fig 7

--- figures/figure7.py
+++ figures/figure7.py
@@ -6,26 +6,27 @@ import numpy as np
 import helpers
 import parameters
 
-pca_range = [parameters.pca_min, parameters.pca_max] # nL/s
-r_range = [parameters.R_min +3e-4, parameters.R_max] # cm, increased the min slightly to avoid convergence issues
-t0_range = [parameters.t0_min, parameters.t0_max] # ms
+pca_range = [parameters.pca_min, parameters.pca_max]  # nL/s
+r_range = [parameters.R_min + 3e-4, parameters.R_max]  # cm, increased the min slightly to avoid convergence issues
+t0_range = [parameters.t0_min, parameters.t0_max]  # ms
 thold = parameters.thold_default
 n_sweep, mult = parameters.cal_n_sweep(thold)
-dR = parameters.dR # cm
+dR = parameters.dR  # cm
 Rh = parameters.Rh
 
 # Set font
-matplotlib.rc('font', family='arial', size = 8)
+matplotlib.rc('font', family='arial', size=8)
 
 fig, axs = plt.subplots(
-    nrows=7, # add 3 new rows
-    ncols=5,  # add 1 new column ...
-    figsize=(6.6, 8),                      
-    gridspec_kw={'width_ratios': [1, 1, 0.01, 1, 1], 
+    nrows=7,  # number of rows
+    ncols=5,  # number of columns
+    figsize=(6.6, 8),
+    gridspec_kw={'width_ratios': [1, 1, 0.01, 1, 1],
                  'height_ratios': [1, 0.01, 1, 0.01, 1, 0.01, 1]}
 )
 
-for ax in axs[:, 2]: 
+# Turn off axes for the 'gaps'
+for ax in axs[:, 2]:
     ax.axis("off")  # Turn off axis lines and labels for this column
 
 for ax in axs[1, :]:
@@ -40,7 +41,7 @@ for ax in axs[5, :]:
 fig_cont = []
 
 for i in range(7):
-    if i == 1 or i ==3 or i==5:
+    if i == 1 or i == 3 or i == 5:
         continue
 
     for j in range(5):
@@ -52,21 +53,20 @@ for i in range(16):
     if i % 2 == 0:
         pass
     else:
-        if i == 9 or i ==11:
+        if i == 9 or i == 11:
             fig_cont[i].set_ylim(0, 10)
         else:
             fig_cont[i].set_ylim(9.7, 10)
 
-fig.text(0.17, 0.97, 'Minimum permeability ($\overline{P}_{Ca}$)', \
-         fontsize = 9, family = 'arial')
-fig.text(0.68, 0.97, 'Maximum permeability ($\overline{P}_{Ca}$)', \
-         fontsize = 9, family = 'arial')
-
+fig.text(0.17, 0.97, 'Minimum permeability ($\\overline{P}_{Ca}$)', \
+         fontsize=9, family='arial')
+fig.text(0.68, 0.97, 'Maximum permeability ($\\overline{P}_{Ca}$)', \
+         fontsize=9, family='arial')
 
 fig_cont[0].set_title('[$Ca^{2+}]_s$ (mM)')
-fig_cont[1].set_title('[B] (mM)')
+fig_cont[1].set_title('[$B]_s$ (mM)')
 fig_cont[2].set_title('[$Ca^{2+}]_s$ (mM)')
-fig_cont[3].set_title('[B] (mM)')
+fig_cont[3].set_title('[$B]_s$ (mM)')
 fig_cont[0].set_ylabel('Min $R_0$, Min $t_0$')
 fig_cont[4].set_ylabel('Min $R_0$, Max $t_0$')
 fig_cont[8].set_ylabel('Max $R_0$, Min $t_0$')
@@ -77,17 +77,25 @@ fig_cont[14].set_xlabel('Time (s)')
 fig_cont[15].set_xlabel('Time (s)')
 
 for f in fig_cont:
-    f.ticklabel_format(axis = 'y', style = 'sci', scilimits = (0, 2))
+    f.ticklabel_format(axis='y', style='sci', scilimits=(0, 2))
 
 gs0 = matplotlib.gridspec.GridSpec(1, 1)
 
-gs0.update(left = 0, right = 0, wspace = 5)
+gs0.update(left=0, right=0, wspace=5)
 
 props = dict(boxstyle='round', facecolor='grey', alpha=0.1)
 for i in range(8):
+    fig_cont[2 * i].text(0.05, 0.9, string.ascii_uppercase[i] + '.1', transform=fig_cont[2 * i].transAxes, weight='bold', bbox=props)
+    fig_cont[2 * i + 1].text(0.05, 0.9, string.ascii_uppercase[i] + '.2', transform=fig_cont[2 * i + 1].transAxes, weight='bold', bbox=props)
 
-    fig_cont[2*i].text(0.05, 0.9, string.ascii_uppercase[i] + '.1', transform = fig_cont[2*i].transAxes, weight = 'bold', bbox=props)
-    fig_cont[2*i+1].text(0.05, 0.9, string.ascii_uppercase[i] + '.2', transform = fig_cont[2*i +1].transAxes, weight = 'bold', bbox=props)
+### Add horizontal and vertical dividing lines ###
+
+# Adding one vertical line between the 2nd and 3rd columns
+fig.add_artist(plt.Line2D([0.52, 0.52], [0.05, 0.925], color='black', linewidth=1))
+
+# Adding three horizontal lines evenly spaced between rows
+for y in [0.25, 0.49, 0.73]:  # Adjust for even spacing between rows
+    fig.add_artist(plt.Line2D([0.08, 0.98], [y, y], color='black', linewidth=1))
 
 i = 0
 for r in r_range:
@@ -107,9 +115,13 @@ for r in r_range:
 for i in range(n_sweep):
     for j in range(16):
         fig_cont[j].axvspan(i*mult, (i+1)*mult, color = 'grey', \
-            alpha = (i%2) * 0.3 + 0.1, transform=fig_cont[j].get_xaxis_transform())      
+            alpha = (i%2) * 0.3 + 0.1, transform=fig_cont[j].get_xaxis_transform())  
+
+### Adjust layout ###
 
 plt.tight_layout()
-plt.subplots_adjust(left = 0.08, bottom = 0.05, right = 0.98, top= 0.925)
-plt.savefig('figures/figure7.pdf')
+plt.subplots_adjust(left=0.08, bottom=0.05, right=0.98, top=0.925)
+plt.savefig('figures/figure7_prac.pdf')
 plt.close()
+
+
